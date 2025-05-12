874b3ab44546b39bea15b9cf20f1e836e0957cca
Peter Matulis <pmatulis@gmail.com>
2025-05-11 22:30:58 -0400

    intersphinx URL
    Signed-off-by: Peter Matulis <pmatulis@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,9 @@ extensions.extend(custom_extensions)
 
 # Intersphinx
 intersphinx_mapping = {
-    "reference": ("https://isovalent-microovn.readthedocs-hosted.com/projects/reference/latest/", None),
+    "v114": ("https://isovalent-microovn.readthedocs-hosted.com/v1.14/", None),
+    "v115": ("https://isovalent-microovn.readthedocs-hosted.com/v1.15/", None),
+    "v116": ("https://isovalent-microovn.readthedocs-hosted.com/v1.16/", None),
 }
 # Prevent resolving of references to external locations.
 # https://www.sphinx-doc.org/usage/extensions/intersphinx.html#confval-intersphinx_disabled_reftypes

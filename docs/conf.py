55dd5ce96834c0c04b933db0a684d16dac2d5490
Peter Matulis <pmatulis@gmail.com>
2025-05-13 01:30:46 -0400

    Configure
    Signed-off-by: Peter Matulis <pmatulis@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,92 +1,85 @@
 import sys
 
-# Configuration file for the Sphinx documentation builder.
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-############################################################
-### Extensions
-############################################################
+# ----------------------------------------------------------
+# Extensions
+# ----------------------------------------------------------
 
 extensions = [
-    'sphinx_design',
-    'sphinx_tabs.tabs',
-    'related-links',
-    'custom-rst-roles',
-    'sphinx_copybutton',
     'myst_parser',
+    'sphinx_tabs.tabs',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.mermaid'
+    'sphinxcontrib.mermaid',
+    # 'feature_maturity_warnings',
+    # 'sphinxext.rediraffe',
+    # 'auth_restricted_section',
+    # 'sphinx.ext.extlinks',
+    # 'sphinx-copybutton',
+    # 'sphinx-notfound-page',
+    # 'side_by_side'
 ]
 
-### Configuration for extensions
-
 # Intersphinx
+# -----------
 intersphinx_mapping = {
     "v114": ("https://isovalent-microovn.readthedocs-hosted.com/v1.14/", None),
     "v115": ("https://isovalent-microovn.readthedocs-hosted.com/v1.15/", None),
     "v116": ("https://isovalent-microovn.readthedocs-hosted.com/v1.16/", None),
-    "v117": ("https://isovalent-microovn.readthedocs-hosted.com/latest/", None),
+    "v117": ("https://isovalent-microovn.readthedocs-hosted.com/latest/", None)
 }
-# Prevent resolving of references to external locations.
-# https://www.sphinx-doc.org/usage/extensions/intersphinx.html#confval-intersphinx_disabled_reftypes
+#   Prevent resolving of references to external locations.
+#   https://www.sphinx-doc.org/usage/extensions/intersphinx.html#confval-intersphinx_disabled_reftypes
 intersphinx_disabled_reftypes = ["*"]
 
-# Mermaid and sphinxcontrib-mermaid
+# Sphinxcontrib-mermaid
+# ---------------------
 mermaid_version = "11.6.0"
 
-# Additional MyST syntax
+# MyST parser
+# -----------
 myst_enable_extensions = [
     'substitution',
-    'deflist',
-    'linkify'
+    'colon_fence'
 ]
 
-############################################################
-### General configuration
-############################################################
+# ----------------------------------------------------------
+# General configuration
+# ----------------------------------------------------------
 
 exclude_patterns = [
     '_build',
     'Thumbs.db',
     '.DS_Store',
-    '.sphinx',
+    '.sphinx'
 ]
 
+## Often-used links
 rst_epilog = '''
 .. include:: /reuse/links.txt
 '''
 
+## reStructuredText and Markdown extensions
 source_suffix = {
     '.rst': 'restructuredtext',
-    '.md': 'markdown',
+    '.md': 'markdown'
 }
 
-# For ignoring specific links
+## Linkchecker
 linkcheck_anchors_ignore_for_url = [
     r'https://github\.com/.*'
 ]
 
-# URLs for the linkchecker to ignore
-linkcheck_ignore = [r'.*cisa\.gov.*']
+linkcheck_ignore = [
+    r'.*cisa\.gov.*'
+]
 
 linkcheck_timeout = 120
 linkcheck_retries = 3
 
-############################################################
-### Styling
-############################################################
+# ----------------------------------------------------------
+# Styling
+# ----------------------------------------------------------
 
-# Find the current builder
-builder = 'dirhtml'
-if '-b' in sys.argv:
-    builder = sys.argv[sys.argv.index('-b')+1]
-
-# Setting templates_path for epub makes the build fail
-if builder == 'dirhtml' or builder == 'html':
-    templates_path = ['.sphinx/_templates']
-
-# Theme configuration
+## Theme configuration
 html_theme = 'furo'
 html_last_updated_fmt = ''
 html_permalinks_icon = '¶'
@@ -95,9 +88,9 @@ html_theme_options = {
     'sidebar_hide_name': True
     }
 
-############################################################
-### Additional files
-############################################################
+# ----------------------------------------------------------
+# Additional files
+# ----------------------------------------------------------
 
 html_static_path = ['.sphinx/_static']
 
